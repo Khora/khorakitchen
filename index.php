390db34071d374b2ec904b74f05767fe9b868bca
caeze <caeze@web.de>
2021-03-02 20:36:53 +0100

    updated and added new texts and store items

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                 <div class="horizontalCenteredBase">
                     <div class="horizontalCentered" style="font-size: 20px; line-height: 30px;">
                         <h3>Hungry in Athens</h3>
-                        2020 was tough. In Athens we have been facing evictions from supported housing for refugees, closures of essential services and escalating police violence. Coronavirus has compounded these problems: community centers and social kitchens all over the city have shut their doors, leaving many of us without access to food or safe spaces. Khora kitchen has stayed open because we know what it means to be hungry - but we need your help to keep going.
+                        2020 was tough. In Athens we have been facing evictions from supported housing for refugees, closures of essential services and escalating police violence. Coronavirus has compounded these problems: community centers and social kitchens all over the city have shut their doors, leaving many without access to food or safe spaces. Khora kitchen has stayed open because we understand what it means to be hungry - but we need your help to keep going. 
                         <br><br><br>
                         <center><a href="<?php echo getFullDomainName(); ?>store.php" class="bigButton">GO TO STORE</a></center><br><br>
                         <h3>Who we are</h3>
@@ -47,15 +47,15 @@
                             "I would describe Khora as a community, but even more I would describe it as a family where we're all siblings with equal status and respect. It's a really warm family where everybody supports everybody else as much as they can."  - Hossein, Iran (Khora Volunteer)
                         </div>
                         <br>
-                        <h3>A cafe for everyone</h3>
-                        We are one of just a few social kitchens that operate in Athens. We currently cook and serve 600 meals every day. We do this without id checks or registration. For many of us, especially those of us who live in camps, life is full of hours-long queues, document checks and rejections to use services on the basis of paperwork, age, or gender. Our cafe is a place where everyone is welcome - no justification necessary.
+                        <h3>Food for everyone</h3>
+                        We are one of just a few social kitchens that operate in Athens. We currently cook and serve 600 meals every day. We do this without id checks or registration. For many people here life is full of hours-long queues, document checks and rejections to use services on the basis of paperwork, age, or gender. Everyone is welcome to access our services - no justification necessary.
                         <br><br>
                          <div style="font-style: italic; font-size: 20px; color: #c4830b; padding: 0px 0px 0px 60px; margin: 0px;">
                             "For me, I love Khora for that. I've been valued here - and not only me, anyone who comes; refugee or not, volunteer or not, wherever you come from, it doesn't matter here. There's no hierarchy, even the structure we have only helps us because it doesn't affect our morale and it doesn't affect how we treat each other" - Brice, Cameroon (Khora Volunteer)
                         </div>
                         <br>
                         <h3>Join us!</h3>
-                        We need your help buying basic ingredients - like oil, vegetables, beans, rice - so that we can keep on sharing these delicious meals with our community. Please head over to our kitchen store page and help us buy what we need!
+                        Head over to our kitchen store page for the basic ingredients that we use every day and support us to keep on sharing these delicious meals with our community.
                         <br><br><br>
                         
                         <center><a href="<?php echo getFullDomainName(); ?>store.php" class="bigButton">GO TO STORE</a></center><br><br>
--- store.php
+++ store.php
@@ -18,7 +18,7 @@
                     echo getTopBanner();
                     $text = '<h3>You buy the food, we serve the meals.</h3>
                             <h3>-</h3>
-                            There are very hungry people in our city. Cuts, exclusionary policies and racism in Greece leave many of us without a stable income or way to get food. We’ve come together as a group to cook hot, healthy and delicious food for everyone. We’ve got the chefs, the kitchen and the energy - we need your help to buy the tomatoes. Together, we the people can feed the people! Scroll down on this page to buy the ingredients we need.';
+                            Many people in Athens don\'t have access to the food they need. Cuts, exclusionary policies and racism in Greece leave many without a stable income or way to get food. Together we cook hot, healthy and delicious food for everyone. We\'ve got the chefs, the kitchen and the energy - we need your help to buy the ingredients. Together, we the people can feed the people! Scroll down on this page to buy the things we need.';
                     $image = 'img/storeTop.jpg';
                     echo getFullWidthTextAndImageBanner($text, $image);
                 ?>
